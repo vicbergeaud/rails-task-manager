678074d4152f549ec57fa5c0d253195123b63513
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-11 15:08:17 +0200

    Exercise 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+	  before_action :set_task, only: [:show, :edit, :update, :destroy]
+	def index
+	 @tasks = Task.all
+	end
+
+	def show 
+	 @task = Task.find(params[:id])
+	end
+
+	def new
+	  @task = Task.new
+	end
+
+	def create
+      @task = Task.new(task_params)
+      @task.save
+      redirect_to tasks_path
+    end
+
+    def update
+      @task = Task.new
+      @task.update(task_params)
+      redirect_to task_path(@task)
+    end
+
+	def edit
+	  @task = Task.find(params[:id])
+	end
+
+	def destroy
+		@task.destroy
+		redirect_to tasks_path
+	end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :details, :completed)
+    end
+
+
+   def set_task
+     @task = Task.find(params[:id])
+   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,2 @@
 class Task < ApplicationRecord
-
-	def index
-		@tasks = Task.all
-	end
-
-	def show 
-		#@tasks = Task.find(params[:id])
-	end
-
 end
